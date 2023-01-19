a17023e4de207d30ed00ed4c8fe290fb94bf6040
Soh Zu Wei <laserklet@gmail.com>
2023-01-19 15:03:12 +0800

    push

--- Rpi/main.py
+++ Rpi/main.py
@@ -4,6 +4,7 @@ def init():
     print("Starting up group 6 rpi")
     #add code below to call a multi processing class to create
     #the different processes and start them all up
+    #tba
 
 
 if __name__ == "__main__":
