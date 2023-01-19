a7ccf024228aa24573c19d4238a6f87fb698afcb
soh zu wei <laserklet@gmail.com>
2023-01-19 14:50:52 +0800

    main file for rpi

---
+++ Rpi/main.py
@@ -0,0 +1,10 @@
+
+
+def init():
+    print("Starting up group 6 rpi")
+    #add code below to call a multi processing class to create
+    #the different processes and start them all up
+
+
+if __name__ == "__main__":
+    init()
