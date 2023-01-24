1cd6b4ca8a6d779d749ea66f0a40235357eda0aa
Soh Zu Wei <laserklet@gmail.com>
2023-01-24 12:03:00 +0800

    multiprocess

--- Rpi/main.py
+++ Rpi/main.py
@@ -1,14 +1,18 @@
-from imageClient import *
+
+from multiprocess import multiprocess
 
 def init():
     print("Starting up group 6 rpi")
     #add code below to call a multi processing class to create
     #the different processes and start them all up
-    imageClient = ImageClient()
-    imageClient.takePhoto()
-    imageClient.stop()
+    multiprocessor = multiprocess()
+    multiprocessor.start()
+
 
     #testing the imagezmq library
+    #imageClient = ImageClient()
+    #imageClient.takePhoto()
+    #imageClient.stop()
 
 
 if __name__ == "__main__":
