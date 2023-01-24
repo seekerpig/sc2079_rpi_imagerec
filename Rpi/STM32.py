6ed3f521ce69374c9e4ccddf8c945f000bab43e7
innuer <103637384+innuer@users.noreply.github.com>
2023-01-24 16:33:07 +0800

    Update STM32.py

--- Rpi/STM32.py
+++ Rpi/STM32.py
@@ -53,24 +53,3 @@ class STM:
 
 
 
-class STM32:
-    def __init__(self):
-        #declare connections
-        print("STM32 init")
-
-    def connect(self):
-        #perform connection
-        print("Connecting STM32..")
-    
-    def disconnect(self):
-        #disconnect
-        print("Disconnecting STM32..")
-
-    
-    def send(self):
-        #sending message
-        print("sending message to STM32")
-
-    def receive(self):
-        #sending message
-        print("receiving message from STM32")
