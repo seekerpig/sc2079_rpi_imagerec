8e3b01c90ec307ad16f73ee82570463c505fd256
Soh Zu Wei <laserklet@gmail.com>
2023-01-25 21:50:00 +0800

    update some basic protocols

---
+++ Rpi/Protocols.py
@@ -0,0 +1,15 @@
+
+
+
+class AlgoMessages:
+    ALGTOSTM = "ALGOTOSTM"
+    
+
+class AndroidMessages:
+    ANDROIDTOALGO = "ANDROIDTOALGO"
+    ANDROIDTOSTM = "ANDROIDTOSTM"
+
+class STMMessages:
+    pass
+
+
