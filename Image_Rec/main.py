87bfcb8dbbf470d42c2d7f4a0d04326857c2f17d
AKASH <44565290+akasht81@users.noreply.github.com>
2023-01-29 20:48:22 +0800

    added yolov5

--- Image_Rec/main.py
+++ Image_Rec/main.py
@@ -1,5 +1,16 @@
 #This is for sending and receiving image from RPI
 from server import *
+import os 
+import shutil
+import time
+import glob
+import torch
+from PIL import Image
+from imutils import paths
+
+def load_model():
+    model = torch.hub.load('./', 'custom', path='./model.pt', source='local')
+    return model
 
 def init():
     print("Initialising image recognition....")
