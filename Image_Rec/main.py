8f5a28df477e5447d6ed186be698141f9248990a
AKASH <44565290+akasht81@users.noreply.github.com>
2023-01-30 14:10:40 +0800

    Using Yolov5 model

--- Image_Rec/main.py
+++ Image_Rec/main.py
@@ -6,17 +6,24 @@ import time
 import glob
 import torch
 from PIL import Image
-from imutils import paths
+#from imutils import paths
 
-def load_model():
-    model = torch.hub.load('./', 'custom', path='./model.pt', source='local')
-    return model
 
 def init():
     print("Initialising image recognition....")
     imageServer = Server()
     imageServer.start()
-    
+
+def load_model():
+    model = torch.hub.load('./', 'custom', path='best.pt', source='local')
+    return model
+
+def predict_image(image_path):
+    model = load_model()
+    img = Image.open(image_path)
+    result = model(img)
+    results.save('runs')
+    return result
 
 
 if __name__ == "__main__":
