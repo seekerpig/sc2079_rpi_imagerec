05b4045c22ebfef986d2ddd86a2f36d692a966f3
Soh Zu Wei <laserklet@gmail.com>
2023-01-30 20:09:23 +0800

    update fixed bugs

--- Rpi/main.py
+++ Rpi/main.py
@@ -6,17 +6,25 @@ from multiprocess import MultiProcess
 #from testingAndroid import MultiProcess
 
 def init():
-    print("Starting up group 6 rpi")
-    #add code below to call a multi processing class to create
-    #the different processes and start them all up
-    multiprocessor = MultiProcess()
-    multiprocessor.start()
+    try:
+        print("Starting up group 6 rpi")
+        #add code below to call a multi processing class to create
+        #the different processes and start them all up
+        multiprocessor = MultiProcess()
+        multiprocessor.start()
 
 
-    #testing the imagezmq library
-    #imageClient = ImageClient()
-    #imageClient.takePhoto()
-    #imageClient.stop()
+        #testing the imagezmq library
+        #imageClient = ImageClient()
+        #imageClient.takePhoto()
+        #imageClient.stop()
+    except KeyboardInterrupt:
+        print("Keyboard interrupted, ending multiprocess")
+        multiprocessor.end()
+    
+    except Exception as error:
+        print("Error has occured: ", error)
+        multiprocessor.end()
 
 
 if __name__ == "__main__":
