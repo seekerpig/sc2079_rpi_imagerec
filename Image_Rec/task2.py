7403172d3c77dd3084b5cf23cc0e2b8a8982fdec
AKASH <44565290+akasht81@users.noreply.github.com>
2023-03-12 02:35:52 +0800

    task2 added

--- Image_Rec/task2.py
+++ Image_Rec/task2.py
@@ -8,6 +8,7 @@ from imutils import paths
 import os
 import time
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -29,24 +30,59 @@ def predict():
         image_bytes = image_file.read()
 
         img = Image.open(io.BytesIO(image_bytes))
+        # Resize the image to a width of 640 pixels while preserving the aspect ratio
+        width, height = img.size
+        aspect_ratio = height / width
+        new_width = 640
+        new_height = int(new_width * aspect_ratio)
+        img = img.resize((new_width, new_height))
+        img.save('raw')
         
-        results = model(img, size=640)  # reduce size=320 for faster inference
+        results = model(img, size=640)
+        df_results = results.pandas().xyxy[0]
+        print(df_results)
+
         results.save('runs')
         stitch_image()
-        # return results.pandas().xyxy[0].to_json(orient="records")
-        df_results = results.pandas().xyxy[0]
-        df_results1 = df_results[df_results['name'] != '41']
+            
+            
+        df_results['bboxHt'] = df_results['ymax'] - df_results['ymin']
+        df_results['bboxWt'] = df_results['xmax'] - df_results['xmin']
+        df_results['bboxArea'] = df_results['bboxHt'] * df_results['bboxWt']
 
+        
+        df_results = df_results.sort_values('bboxArea', ascending=True)  # Label with largest bbox height will be last
+        
+        if len(df_results)>1:
+            if abs(df_results['ymax'][0] - df_results['ymax'][1]) <=20 or abs(df_results['ymin'][0] - df_results['ymin'][1]) <=20 or abs(df_results['xmin'][0] - df_results['xmin'][1]) <=20 or abs(df_results['xmax'][0] - df_results['xmax'][1]) <=20:
+                print("Yes")
+                df_results = df_results.sort_values('confidence', ascending=True)  # Label with largest bbox height will be last
+        
+        if len(df_results)>=1:
+            pred1 = df_results['name'].to_numpy()
+            print(pred1)           
+            if pred1[-1] == '18' or pred1[-1] == '28':
+                results = model1(img, size=640)
+                df_results = results.pandas().xyxy[0]
+                
+                df_results['bboxHt'] = df_results['ymax'] - df_results['ymin']
+                df_results['bboxWt'] = df_results['xmax'] - df_results['xmin']
+                df_results['bboxArea'] = df_results['bboxHt'] * df_results['bboxWt']
 
-        df_results1['bboxHt'] = df_results1['ymax'] - df_results1['ymin']
-        df_results1['bboxWt'] = df_results1['xmax'] - df_results1['xmin']
-        df_results1['bboxArea'] = df_results1['bboxHt'] * df_results1['bboxWt']
-
-        df_results1 = df_results1.sort_values('bboxArea', ascending=True)  # Label with largest bbox height will be last
-        print(df_results1)
-        pred_list = df_results1['name'].to_numpy()
+        
+                df_results = df_results.sort_values('bboxArea', ascending=True)  # Label with largest bbox height will be last
+        
+                if len(df_results)>1:
+                    if abs(df_results['ymax'][0] - df_results['ymax'][1]) <=20 or abs(df_results['ymin'][0] - df_results['ymin'][1]) <=20 or abs(df_results['xmin'][0] - df_results['xmin'][1]) <=20 or abs(df_results['xmax'][0] - df_results['xmax'][1]) <=20:
+                        print("Yes")
+                        df_results = df_results.sort_values('confidence', ascending=True)  # Label with largest bbox height will be last
+                        print("yes1") 
+            
+            
+        
+        print(df_results)
+        pred_list = df_results['name'].to_numpy()
         pred = 'NA'
-
 
         if pred_list.size > 0:
             for i in pred_list:
@@ -56,41 +92,13 @@ def predict():
 
         Symbol_Map_to_id = {
             "NA": 'NA',
-            "11": 36, #1
-            "12": 38, #2
-            "13": 39, #3
-            "14": 37, #4
-            "15": 15, #5
-            "16": 36, #6
-            "17": 38,
-            "18": 18,
-            "19": 37,
-            "20": 20,
-            "21": 36,
-            "22": 38,
-            "23": 23,
-            "24": 38,
-            "25": 25,
-            "26": 36,
-            "27": 38,
-            "28": 28,
-            "29": 38,
-            "30": 30,
-            "31": 36,
-            "32": 38,
-            "33": 33,
-            "34": 38,
-            "35": 35,
-            "36": 36,
-            "37": 37,
             "38": 38,
             "39": 39,
-            "40": 40,
-            "41": 41
         }
 
         image_id = Symbol_Map_to_id.get(pred, 'NA')
         result = {"image_id": image_id}
+        print(result)
 
         return jsonify(result)
 
@@ -115,13 +123,16 @@ def stitch_image():
 
 
 def load_model():
-    model = torch.hub.load('./yolov5/', 'custom', path='yolov5/bestyy.pt', source='local')
-    return model
+    model = torch.hub.load('./yolov5/', 'custom', path='yolov5/bestyzs', source='local')
+    model1 = torch.hub.load('./yolov5/', 'custom', path='yolov5/best8', source='local')
+    return model, model1 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
     parser.add_argument("--port", default=5005, type=int, help="port number")
     args = parser.parse_args()
-    model = load_model()
+    model_tuple = load_model()
+    model = model_tuple[0]
+    model1 = model_tuple[1]
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
